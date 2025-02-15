345cba74ee29b59d041afc3375fd5fa46c73d78e
Nawar <nawabsn911@gmail.com>
2025-02-15 10:07:26 +0100

    fix deploy issus

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,4 +1,4 @@
 import mobileMenuIcon from "./mobileMenuIcon.svg";
-import arrowUpDownIcon from "./arrowupDown.svg";
+import arrowUpDownIcon from "./arrowUpDown.svg";
 import goIcon from "./go.svg";
 export { mobileMenuIcon, arrowUpDownIcon, goIcon };
